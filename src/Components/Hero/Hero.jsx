325471dadbd77dcaa0a7a297c65a48a41e787f54
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-04-06 23:44:13 +0200

    Add playsInline attribute to video

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import video from "../../assets/university_background.mp4"
 export const Hero = () => {
   return (
     <div className={styles.hero}>
-              <video src={video} autoPlay muted controls loop className={styles.backgroundVedio}></video>
+        <video src={video} autoPlay muted controls loop playsInline className={styles.backgroundVedio}></video>
       <div className={styles.heroText}>
 
         <h1>We ensure better education for better world</h1>
